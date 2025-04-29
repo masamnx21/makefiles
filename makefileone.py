4b88cf35c1bfbcd6e1877e46da588dda7b1a4a7d
Nel M <masamnx21@juniata.edu>
2025-04-29 08:31:51 -0400

    add makefileone

---
+++ makefileone.py
@@ -0,0 +1,11 @@
+if __name__=='__main__':
+
+	#making output file named makefileone
+	outfile = open('makefileone.txt', 'w')
+
+	#outputting simple phrases to makefileone
+	outfile.write('Hello Dr. Kruse! \n')
+	outfile.write('Today is Tuesday, April 29, 2025. \n')
+	outfile.write('This is the first file for the MACS-341 makefile assignment. \n')
+	outfile.write('It is almost the end of classes! \n')
+	outfile.write('I am excited for summer break. \n')
